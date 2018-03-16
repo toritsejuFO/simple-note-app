03bc4fc106b0dd96c58923b52c1c2346dccfed71
Faith Odonghanro <toriboi.fo@gmail.com>
2018-03-16 07:02:59 +0100

    Simple Not App

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const {render} = ReactDOM;
 
 // include locally stored notes for display
 
-var id = [
+var ids = [
 (localStorage.getItem('id1') == null) ? localStorage.setItem('id1', '1') : localStorage.getItem('id1'),
 (localStorage.getItem('id2') == null) ? localStorage.setItem('id2', '2') : localStorage.getItem('id2'),
 (localStorage.getItem('id3') == null) ? localStorage.setItem('id3', '3') : localStorage.getItem('id3'),
@@ -11,7 +11,7 @@ var id = [
 (localStorage.getItem('id6') == null) ? localStorage.setItem('id6', '6') : localStorage.getItem('id6'),
 ];
 
-var title = [
+var titles = [
   (localStorage.getItem('title1') == null) ? localStorage.setItem('title1', 'Heading1') : localStorage.getItem('title1'),
   (localStorage.getItem('title2') == null) ? localStorage.setItem('title2', 'Heading2') : localStorage.getItem('title2'),
   (localStorage.getItem('title3') == null) ? localStorage.setItem('title3', 'Heading3') : localStorage.getItem('title3'),
@@ -20,7 +20,7 @@ var title = [
   (localStorage.getItem('title6') == null) ? localStorage.setItem('title6', 'Heading6') : localStorage.getItem('title6'),
 ];
 
-var body = [
+var bodies = [
 (localStorage.getItem('body1') == null) ? localStorage.setItem('body1', 'Type your note here') : localStorage.getItem('body1'),
 (localStorage.getItem('body2') == null) ? localStorage.setItem('body2', 'Type your note here') : localStorage.getItem('body2'),
 (localStorage.getItem('body3') == null) ? localStorage.setItem('body3', 'Type your note here') : localStorage.getItem('body3'),
@@ -28,6 +28,10 @@ var body = [
 (localStorage.getItem('body5') == null) ? localStorage.setItem('body5', 'Type your note here') : localStorage.getItem('body5'),
 (localStorage.getItem('body6') == null) ? localStorage.setItem('body6', 'Type your note here') : localStorage.getItem('body6'),
 ];
+
+if(localStorage.getItem('lastSavedID') == null) {
+  localStorage.setItem('lastSavedID', 1);
+}
 
 // My App's React Components
 
@@ -39,53 +43,41 @@ function Header(props) {
   );
 }
 
-function Note(props) {
-  return (
-    <div className="Note">
-      <h4 className="MiniTitle">{props.id}. {props.title}</h4>
-      <p className="MiniBody">{props.body}
-        <button className="editButton btn btn-gray">
-          Edit
-        </button>
-      </p>
-    </div>
-  );
-}
-
-class List extends React.Component {
+// Note Component
+class Note extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const titles = this.props.noteTitle;
-    const bodies = this.props.noteBody;
-    const notes = [];
-
-    titles.forEach((elm, i) =>
-      notes[i] = <Note key={i} id={(i+1).toString()} title={elm} body={bodies[i]} />
-    );
-
     return (
-      <div className="List col-md-4 col-4">
-        {notes}
-
-        <p className="notice">Refresh to see edited changes take effect in your list of notes</p>
+      <div className="Note col-md-10  col-sm-10 col-8">
+        <h4 className="MiniTitle">{this.props.id}. {this.props.title.substr(0, 10)} ...</h4>
+        <p className="MiniBody">{this.props.body.substr(0, 18)} ...</p>
       </div>
     );
   }
 }
 
-class EditNote extends React.Component {
+
+
+// Main App component, whole interface containg form(controlled)
+class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // Save the id of the note updated last, so on refresh form state is retained
+    const lastSavedID = localStorage.getItem('lastSavedID');
+
     this.state = {
-      title: this.props.title,
-      body: this.props.body,
+      title: localStorage.getItem("title" + lastSavedID),
+      body: localStorage.getItem("body" + lastSavedID),
+      id: localStorage.getItem("id" + lastSavedID),
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.editClickedNote = this.editClickedNote.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -101,76 +93,99 @@ class EditNote extends React.Component {
     );
   }
 
+  // Chnage the state of form input values to currently clicked note
+  editClickedNote(e) {
+    const noteID = e.target.value / 10;
+    console.log(noteID);
+
+    this.setState({
+      title: localStorage.getItem("title" + noteID),
+      body: localStorage.getItem("body" + noteID),
+      id: localStorage.getItem("id" + noteID),
+    });
+  }
+
+  // Update saved note
   handleSubmit(e) {
-    if(this.state.body == ""){
-      alert("Cannot save empty note");
+    if(this.state.body == "" || this.state.title == ""){
+      alert("Cannot save note with empty title or body");
     }
     else{
-      var title = "title" + this.props.id;
-      var body = "body" + this.props.id;
+      var title = "title" + this.state.id;
+      var body = "body" + this.state.id;
       localStorage.setItem(title, this.state.title);
       localStorage.setItem(body, this.state.body);
+      localStorage.setItem("lastSavedID", this.state.id);
     }
     e.preventDefault();
   }
 
   render() {
-    return(
-      <form className="EditNote form form-control" onSubmit={this.handleSubmit}>
-        <div>
-          <p>You are editing slot <input className="UneditableID" type="number" value={this.props.id} disabled/></p>
-        </div>
-        <div>
-          <input className="Title form-control"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleTitleChange}
-          />
-        </div>
-        <div>
-          <textarea className="Body form-control"
-            onChange={this.handleBodyChange}
-            value={this.state.body}
-          />
-        </div>
-        <div>
-          <input type="submit" value="Save" className="SaveButton btn btn-gray float-right" />
-        </div>
-      </form>
+    const notes = [];
+
+    // Attach a button to for each note to be rendered
+    titles.forEach((elm, i) =>
+      notes[i] =
+      <div key={i+1} className="NoteButtonDiv row">
+        <Note key={i+1}
+          id={(i+1).toString()}
+          title={elm}
+          body={bodies[i]}
+        />
+        <button key={(i+1)*10}
+          className="editButton btn btn-gray col-md-2 col-sm-2 col-4"
+          value={(i+1)*10}
+          onClick={this.editClickedNote}
+        >
+          Edit
+        </button>
+      </div>
     );
-  }
-}
 
-// Main App component, whole interface
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: localStorage.getItem("title1"),
-      body: localStorage.getItem("body1"),
-      id: localStorage.getItem("id1"),
-    };
-  }
-
-  render() {
     return (
-      <div className="App container">
+      <div className="App container-fluid">
         <div className="row">
           <Header text="My Simple Note App"/>
         </div>
 
         <div className="main row">
-          <List noteTitle={title} noteBody={body} />
+          <div className="List col-md-4 col-sm-5 col-12">
+            {notes}
+            <p className="notice">
+              Refresh to see edited changes take effect in your list of notes.
+              And ensure to save an edited note before editing another note.
+            </p>
+          </div>
 
-          <div className="NewNote col-md-8 col-8">
-            <EditNote title={this.state.title} body={this.state.body} id={this.state.id}/>
+          <div className="NewNote col-md-8 col-sm-7 col-12">
+            {/* Form to edit and update existing notes*/}
+            <form className="EditNote form form-control" onSubmit={this.handleSubmit}>
+              <div>
+                <p>You are editing slot <input className="UneditableID" type="number" value={this.state.id} disabled/></p>
+              </div>
+              <div>
+                <input className="Title form-control"
+                  value={this.state.title}
+                  onChange={this.handleTitleChange}
+                />
+              </div>
+              <div>
+                <textarea className="Body form-control"
+                  onChange={this.handleBodyChange}
+                  value={this.state.body}
+                />
+              </div>
+              <div>
+                <input type="submit" value="Save" className="SaveButton btn btn-gray float-right" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
     );
   }
 }
+
 
 // Render App component
 render(
