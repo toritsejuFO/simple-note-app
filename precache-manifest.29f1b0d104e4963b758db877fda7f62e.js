5870071a47da1964f16d9393a1525e286bee8bcb
Faith Odonghanro <toriboi.fo@gmail.com>
2019-01-31 03:19:11 +0100

    Updates

---
+++ precache-manifest.29f1b0d104e4963b758db877fda7f62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4746fce2fb5305db6ed1",
+    "url": "/simpleNoteApp/static/js/runtime~main.4746fce2.js"
+  },
+  {
+    "revision": "20ab7451730f691078cb",
+    "url": "/simpleNoteApp/static/js/main.20ab7451.chunk.js"
+  },
+  {
+    "revision": "32f3431e6ba533cd2029",
+    "url": "/simpleNoteApp/static/js/1.32f3431e.chunk.js"
+  },
+  {
+    "revision": "20ab7451730f691078cb",
+    "url": "/simpleNoteApp/static/css/main.e585240c.chunk.css"
+  },
+  {
+    "revision": "32f3431e6ba533cd2029",
+    "url": "/simpleNoteApp/static/css/1.96610c7b.chunk.css"
+  },
+  {
+    "revision": "29099b794051d6af9a7ffd8774061c3b",
+    "url": "/simpleNoteApp/index.html"
+  }
+];
